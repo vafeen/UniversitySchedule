e86fc94b9eff9e567355ba28163d65d8747dc7b3
A <666av6@gmail.com>
2024-07-05 10:07:30 +0300

    Add implementation hilt, room

---
+++ app/src/main/java/ru/vafeen/universityschedule/main/App.kt
@@ -0,0 +1,9 @@
+package ru.vafeen.universityschedule.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+}
--- app/src/main/java/ru/vafeen/universityschedule/main/MainActivity.kt
+++ app/src/main/java/ru/vafeen/universityschedule/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package ru.vafeen.universityschedule
+package ru.vafeen.universityschedule.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 import ru.vafeen.universityschedule.ui.theme.UniversityScheduleTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
