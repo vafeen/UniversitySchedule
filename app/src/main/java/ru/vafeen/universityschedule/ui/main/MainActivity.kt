dbdc656a3aab1eddd75f5fc4a3111de4e0cebc7a
A <666av6@gmail.com>
2024-07-06 07:11:49 +0300

    Add custom theme

--- app/src/main/java/ru/vafeen/universityschedule/ui/main/MainActivity.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/main/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import ru.vafeen.universityschedule.ui.theme.MainTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MainTheme {
 
+            }
         }
     }
 }
--- app/src/main/java/ru/vafeen/universityschedule/ui/theme/Theme.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/theme/Theme.kt
@@ -1,58 +1,50 @@
 package ru.vafeen.universityschedule.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+data class ScheduleColors(
+    val mainColor: Color,
+    val background: Color,
+    val text: Color,
+    val buttonColor: Color,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val basePalette = ScheduleColors(
+    mainColor = Color(0xFF0E568D),
+    background = Color.White,
+    text = Color.Black,
+    buttonColor = Color.DarkGray
 )
+val baseDarkPalette = basePalette.copy(background = Color.Black)
 
 @Composable
-fun UniversityScheduleTheme(
+fun MainTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        basePalette
+    } else {
+        baseDarkPalette
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        LocalColors provides colors,
         content = content
     )
+}
+
+object ScheduleTheme {
+    val colors: ScheduleColors
+        @Composable
+        get() = LocalColors.current
+}
+
+val LocalColors = staticCompositionLocalOf<ScheduleColors> {
+    error("Composition error")
 }
