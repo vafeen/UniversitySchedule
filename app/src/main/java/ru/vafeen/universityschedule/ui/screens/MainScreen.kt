aa0c69c7aa2edbf43b6c8d3fadce4b9e090c56c3
Vafeen <666av6@gmail.com>
2024-07-06 08:09:48 +0300

    Add some parts of layout MainScreen

--- app/src/main/java/ru/vafeen/universityschedule/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/screens/MainScreen.kt
@@ -1,11 +1,61 @@
 package ru.vafeen.universityschedule.ui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(navController: NavController) {
-    Text(text = "mainScreen")
+    var localDateTime by remember {
+        mutableStateOf(LocalDateTime.now())
+    }
+
+    val pagerState = rememberPagerState(
+        pageCount = {
+            7
+        }, initialPage = localDateTime.dayOfWeek.value - 1
+    )
+    var nowIsLesson: Boolean
+    LaunchedEffect(key1 = null) {
+        withContext(Dispatchers.Main) {
+            while (true) {
+                localDateTime = LocalDateTime.now()
+                delay(1000L)
+            }
+        }
+    }
+    Scaffold(
+
+    ) { paddingValues: PaddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = paddingValues)
+        ) {
+            Text(text = "hello world")
+        }
+    }
+
 }
