4ad160270067a300fec7e81c6c0caf0a7db99201
Vafeen <666av6@gmail.com>
2024-07-06 08:05:09 +0300

    Add FontSize

---
+++ app/src/main/java/ru/vafeen/universityschedule/ui/theme/FontSize.kt
@@ -0,0 +1,11 @@
+package ru.vafeen.universityschedule.ui.theme
+
+import androidx.compose.ui.unit.sp
+
+object FontSize {
+    val extraSmall = 10.sp
+    val small = 15.sp
+    val medium = 17.sp
+    val big = 19.sp
+    val huge = 22.sp
+}
