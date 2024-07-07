2bf143a59b641e8742d2210e6ccac972cfe3753c
Vafeen <666av6@gmail.com>
2024-07-07 08:58:10 +0300

    Add creating GSheetsService on MainScreen

--- app/src/main/java/ru/vafeen/universityschedule/ui/navigation/AppNavHost.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/navigation/AppNavHost.kt
@@ -2,11 +2,13 @@ package ru.vafeen.universityschedule.ui.navigation
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ru.vafeen.universityschedule.ui.components.screens.MainScreen
 import ru.vafeen.universityschedule.ui.components.screens.SettingsScreen
+import ru.vafeen.universityschedule.ui.components.viewModels.MainScreenViewModel
 
 @Composable
 fun AppNavHost(context: Context) {
@@ -15,7 +17,11 @@ fun AppNavHost(context: Context) {
         navController = navController, startDestination = Screen.Main.route
     ) {
         composable(Screen.Main.route) {
-            MainScreen(navController = navController)
+            MainScreen(
+                navController = navController,
+                viewModel = viewModel(modelClass = MainScreenViewModel::class.java),
+                context = context
+            )
         }
         composable(Screen.Settings.route) {
             SettingsScreen(navController = navController, context = context)
