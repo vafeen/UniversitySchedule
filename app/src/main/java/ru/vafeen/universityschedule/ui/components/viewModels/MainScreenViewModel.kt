73abc47d253de19dc018f0578f1931f166c5ad6c
Vafeen <666av6@gmail.com>
2024-07-07 05:05:37 +0300

    Update MainScreen with ViewModel

--- app/src/main/java/ru/vafeen/universityschedule/ui/components/viewModels/MainScreenViewModel.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/components/viewModels/MainScreenViewModel.kt
@@ -1,9 +1,20 @@
 package ru.vafeen.universityschedule.ui.components.viewModels
 
 import androidx.lifecycle.ViewModel
+import ru.vafeen.universityschedule.noui.lesson_additions.Frequency
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.util.Calendar
 import javax.inject.Inject
 
 class MainScreenViewModel @Inject constructor(
 ) : ViewModel() {
+    val ruDaysOfWeek = listOf("ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС")
+    val daysOfWeek = DayOfWeek.entries.toList()
+    val weekOfYear =
+        if (Calendar.getInstance().get(Calendar.WEEK_OF_YEAR) % 2 == 0) Frequency.Denominator
+        else Frequency.Numerator
+    val todayDate: LocalDate = LocalDate.now()
+    var nowIsLesson: Boolean = false
 
 }
---
+++ app/src/main/java/ru/vafeen/universityschedule/utils/DateTimeUtils.kt
@@ -0,0 +1,33 @@
+package ru.vafeen.universityschedule.utils
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+operator fun LocalTime.compareTo(localDateTime: LocalDateTime): Int {
+    return this.compareTo(LocalTime.of(localDateTime.hour, localDateTime.minute))
+}
+
+fun LocalDate.getDateString(): String {
+    return "${dayOfMonth}." + if (month.value < 10) {
+        "0${month.value}"
+    } else {
+        month.value
+    }
+}
+
+fun LocalTime.getTimeStringAsHMS(): String {
+    return if (hour < 10) {
+        "0$hour"
+    } else {
+        "$hour"
+    } + ":" + if (minute < 10) {
+        "0$minute"
+    } else {
+        "$minute"
+    } + ":" + if (second < 10) {
+        "0$second"
+    } else {
+        "$second"
+    }
+}
