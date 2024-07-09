7c379c1a5d4f34ff896d3fdf274b7031adf12205
Vafeen <666av6@gmail.com>
2024-07-09 05:18:28 +0300

    Add DatabaseRepository injecting to MainScreenViewModel

---
+++ app/src/main/java/ru/vafeen/universityschedule/database/dao/DatabaseRepository.kt
@@ -0,0 +1,19 @@
+package ru.vafeen.universityschedule.database.dao
+
+import kotlinx.coroutines.flow.Flow
+import ru.vafeen.universityschedule.database.entity.Lesson
+import javax.inject.Inject
+
+class DatabaseRepository @Inject constructor(
+    private val lessonDao: LessonDao
+) {
+
+    fun getAllAsFlowLessons(): Flow<List<Lesson>> = lessonDao.getAllAsFlow()
+
+    suspend fun insertAllLessons(vararg entities: Lesson) = lessonDao.insertAll(entities = entities)
+
+    suspend fun deleteAllLessons(vararg entities: Lesson) = lessonDao.delete(entities = entities)
+
+    suspend fun updateAllLessons(vararg entities: Lesson) = lessonDao.update(entities = entities)
+
+}
