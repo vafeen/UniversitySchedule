15ee0be85d37af171042e807e2e15648d9d559b0
A <666av6@gmail.com>
2024-07-09 12:53:09 +0300

    Remove unuseful GSonConverterFactory

--- app/src/main/java/ru/vafeen/universityschedule/utils/GSheetsUtils.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/GSheetsUtils.kt
@@ -1,13 +1,11 @@
 package ru.vafeen.universityschedule.utils
 
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.vafeen.universityschedule.network.GSheetsService
 
 fun createGSheetsService(link: String): GSheetsService? = try {
     Retrofit.Builder()
         .baseUrl(link.replace("edit?usp=sharing", ""))
-        .addConverterFactory(GsonConverterFactory.create())
         .build().create(GSheetsService::class.java)
 } catch (e: Exception) {
     null
