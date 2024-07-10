b6ddea1e41a4636caf5cca19ce4591459dcff8a0
A <666av6@gmail.com>
2024-07-10 06:21:47 +0300

    Replace link

--- app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
@@ -1,6 +1,6 @@
 package ru.vafeen.universityschedule.utils
 
 object Link {
-    const val CODE: String = "https://github.com/vafeen/MySchedule"
+    const val CODE: String = "https://github.com/vafeen/UniversitySchedule"
     const val LICENSE: String = "https://www.apache.org/licenses/LICENSE-2.0"
 }
