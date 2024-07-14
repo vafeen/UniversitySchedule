084c154af61ea38f6a4e3a8ed4e7a6d59e238e59
Vafeen <666av6@gmail.com>
2024-07-14 15:20:18 +0300

    Improve colors of interface

--- app/src/main/java/ru/vafeen/universityschedule/ui/components/ColorPickerDialogButton.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/components/ColorPickerDialogButton.kt
@@ -1,22 +1,16 @@
 package ru.vafeen.universityschedule.ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
-import androidx.compose.material3.Text
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import ru.vafeen.universityschedule.ui.theme.ScheduleTheme
+import ru.vafeen.universityschedule.utils.suitableColor
 
 @Composable
 fun ColorPickerDialogButton(
@@ -26,12 +20,18 @@ fun ColorPickerDialogButton(
     enabled: Boolean = true,
     content: @Composable RowScope.() -> Unit
 ) {
-    Button(
+    OutlinedButton(
         shape = RoundedCornerShape(10.dp),
         enabled = enabled,
-        colors = ButtonDefaults.buttonColors(containerColor = color),
+        colors = ButtonDefaults.outlinedButtonColors(
+            containerColor = color,
+            contentColor = color.suitableColor(),
+            disabledContentColor = color.suitableColor().copy(alpha = 0.5f),
+            disabledContainerColor = color.copy(alpha = 0.5f),
+        ),
         modifier = modifier,
         onClick = onClick,
-        content = content
+        content = content,
+        border = BorderStroke(1.dp, ScheduleTheme.colors.oppositeTheme)
     )
 }
