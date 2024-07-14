e72dabd1dfe8f64cd5e66f73e6f3d3e08a7ae2b0
A <666av6@gmail.com>
2024-07-14 16:04:41 +0300

    Add colors to separate file

--- app/src/main/java/ru/vafeen/universityschedule/ui/theme/Color.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/theme/Color.kt
@@ -2,10 +2,5 @@ package ru.vafeen.universityschedule.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val mainLightColor = Color(0xFF68BD45)
+val mainDarkColor = Color(0xFF161617)
