7da49f6fc3f428168b89f65dfd72b653ce5130a0
Vafeen <666av6@gmail.com>
2024-07-14 15:56:51 +0300

    Add some docs

--- app/src/main/java/ru/vafeen/universityschedule/database/DatabaseRepository.kt
+++ app/src/main/java/ru/vafeen/universityschedule/database/DatabaseRepository.kt
@@ -11,10 +11,21 @@ class DatabaseRepository @Inject constructor(
 
     fun getAllAsFlowLessons(): Flow<List<Lesson>> = lessonDao.getAllAsFlow()
 
+    /**
+     * Inserting && Updating in database one or more lessons
+     * @param entities [Set of entities to put in database]
+     */
     suspend fun insertAllLessons(vararg entities: Lesson) = lessonDao.insertAll(entities = entities)
 
+    /**
+     * Deleting from database one or more lessons
+     * @param entities [Set of entities to remove from database]
+     */
     suspend fun deleteAllLessons(vararg entities: Lesson) = lessonDao.delete(entities = entities)
 
+    /**
+     * Updating in database one or more lessons
+     * @param entities [Set of entities to update in database]
+     */
     suspend fun updateAllLessons(vararg entities: Lesson) = lessonDao.update(entities = entities)
-
 }
--- app/src/main/java/ru/vafeen/universityschedule/utils/ColorUtils.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/ColorUtils.kt
@@ -4,7 +4,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.graphics.ColorUtils
 
-
+/**
+ * Getting suitable color
+ * @param Color [For color which we will get a suitable color]
+ */
 fun Color.suitableColor(): Color =
     if (this.isDark()) Color.White else Color.Black
 
