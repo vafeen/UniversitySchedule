3a1190d2db9584d300f097f4edde081a93f098dc
A <666av6@gmail.com>
2024-07-22 14:59:46 +0300

    Replace padding for creating border on BorderStroke

--- app/src/main/java/ru/vafeen/universityschedule/ui/components/ui_utils/EditLinkDialog.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/components/ui_utils/EditLinkDialog.kt
@@ -2,6 +2,7 @@ package ru.vafeen.universityschedule.ui.components.ui_utils
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -50,111 +51,104 @@ fun EditLinkDialog(
         onDismissRequest = { onDismissRequest() }, properties = DialogProperties()
     ) {
         Card(
-            colors =
-            CardDefaults.cardColors(containerColor = ScheduleTheme.colors.oppositeTheme)
+            colors = CardDefaults.cardColors(
+                containerColor = ScheduleTheme.colors.singleTheme
+            ),
+            border = BorderStroke(width = 2.dp, color = ScheduleTheme.colors.oppositeTheme)
         ) {
-            Card(
+            Column(
                 modifier = Modifier
-                    .padding(3.dp),
-
-                colors = CardDefaults.cardColors(containerColor = ScheduleTheme.colors.singleTheme)
+                    .height(250.dp), verticalArrangement = Arrangement.SpaceAround
             ) {
-                Column(
+                IconButton(
                     modifier = Modifier
-                        .height(250.dp), verticalArrangement = Arrangement.SpaceAround
+                        .padding(horizontal = 5.dp)
+                        .align(Alignment.End),
+                    onClick = { onDismissRequest() },
                 ) {
-                    IconButton(
-                        modifier = Modifier
-                            .padding(horizontal = 5.dp)
-                            .align(Alignment.End),
-                        onClick = { onDismissRequest() },
-                    ) {
-                        Icon(
-                            modifier = Modifier.size(30.dp),
-                            imageVector = Icons.Default.Close,
-                            contentDescription = "close",
-                            tint = ScheduleTheme.colors.oppositeTheme
-                        )
-                    }
-
-                    TextForThisTheme(
-                        text = settings.link ?: stringResource(id = R.string.no_link),
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .weight(1f)
-                            .padding(horizontal = 5.dp),
-                        fontSize = FontSize.medium
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "close",
+                        tint = ScheduleTheme.colors.oppositeTheme
                     )
+                }
+
+                TextForThisTheme(
+                    text = settings.link ?: stringResource(id = R.string.no_link),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .weight(1f)
+                        .padding(horizontal = 5.dp),
+                    fontSize = FontSize.medium
+                )
 
 
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        if (settings.link != null) {
-                            IconButton(
-                                onClick = {
-                                    settings.link?.let {
-                                        context.copyTextToClipBoard(
-                                            text = it
-                                        )
-                                    }
-                                }
-                            ) {
-                                Icon(
-                                    modifier = Modifier.size(iconsSize),
-                                    painter = painterResource(id = R.drawable.copy),
-                                    contentDescription = "copy",
-                                    tint = ScheduleTheme.colors.oppositeTheme
-                                )
-                            }
-                            IconButton(
-                                onClick = {
-                                    settings = settings.copy(link = null).save(
-                                        sharedPreferences = sharedPreferences
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (settings.link != null) {
+                        IconButton(
+                            onClick = {
+                                settings.link?.let {
+                                    context.copyTextToClipBoard(
+                                        text = it
                                     )
                                 }
-                            ) {
-                                Icon(
-                                    modifier = Modifier.size(iconsSize),
-                                    painter = painterResource(id = R.drawable.clear),
-                                    contentDescription = "clear",
-                                    tint = ScheduleTheme.colors.oppositeTheme
-                                )
                             }
-                        }
-                        IconButton(onClick = {
-                            context.pasteText()?.let {
-                                val contains = it.contains("docs.google.com/spreadsheets/")
-                                if (contains)
-                                    settings = settings.copy(
-                                        link = if (!it.contains(Link.PROTOCOL))
-                                            "${Link.PROTOCOL}$it" else it
-                                    ).save(sharedPreferences = sharedPreferences)
-                                else
-                                    Toast.makeText(
-                                        context,
-                                        context.getString(R.string.its_no_google_sheets_link),
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-
-                            }
-                        }) {
+                        ) {
                             Icon(
                                 modifier = Modifier.size(iconsSize),
-                                painter = painterResource(id = R.drawable.paste),
-                                contentDescription = "paste",
+                                painter = painterResource(id = R.drawable.copy),
+                                contentDescription = "copy",
+                                tint = ScheduleTheme.colors.oppositeTheme
+                            )
+                        }
+                        IconButton(
+                            onClick = {
+                                settings = settings.copy(link = null).save(
+                                    sharedPreferences = sharedPreferences
+                                )
+                            }
+                        ) {
+                            Icon(
+                                modifier = Modifier.size(iconsSize),
+                                painter = painterResource(id = R.drawable.clear),
+                                contentDescription = "clear",
                                 tint = ScheduleTheme.colors.oppositeTheme
                             )
                         }
                     }
+                    IconButton(onClick = {
+                        context.pasteText()?.let {
+                            val contains = it.contains("docs.google.com/spreadsheets/")
+                            if (contains)
+                                settings = settings.copy(
+                                    link = if (!it.contains(Link.PROTOCOL))
+                                        "${Link.PROTOCOL}$it" else it
+                                ).save(sharedPreferences = sharedPreferences)
+                            else
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.its_no_google_sheets_link),
+                                    Toast.LENGTH_SHORT
+                                ).show()
 
+                        }
+                    }) {
+                        Icon(
+                            modifier = Modifier.size(iconsSize),
+                            painter = painterResource(id = R.drawable.paste),
+                            contentDescription = "paste",
+                            tint = ScheduleTheme.colors.oppositeTheme
+                        )
+                    }
                 }
+
             }
         }
-
-
     }
 }
