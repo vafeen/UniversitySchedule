089cb477f5e58c467aa2f1fcef5baab9909cc4ac
A <666av6@gmail.com>
2024-08-14 21:07:50 +0300

    Edit some sections of SettingsScreen.kt

--- app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
@@ -2,7 +2,6 @@ package ru.vafeen.universityschedule.utils
 
 object Link {
     const val CODE: String = "https://github.com/vafeen/UniversitySchedule"
-    const val LICENSE: String = "https://www.apache.org/licenses/LICENSE-2.0"
     const val VK_GROUP = "https://vk.ru/universityschedule"
     const val PROTOCOL: String = "https://"
 }
