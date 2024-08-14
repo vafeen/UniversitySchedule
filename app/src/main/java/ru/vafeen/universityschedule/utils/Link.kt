0e4e7e7087c684d65404f4ea4fd2aa42f2700fb2
A <666av6@gmail.com>
2024-08-14 20:51:40 +0300

    Add project community tab to SettingsScreen.kt

--- app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/Link.kt
@@ -3,5 +3,6 @@ package ru.vafeen.universityschedule.utils
 object Link {
     const val CODE: String = "https://github.com/vafeen/UniversitySchedule"
     const val LICENSE: String = "https://www.apache.org/licenses/LICENSE-2.0"
+    const val VK_GROUP = "https://vk.ru/universityschedule"
     const val PROTOCOL: String = "https://"
 }
