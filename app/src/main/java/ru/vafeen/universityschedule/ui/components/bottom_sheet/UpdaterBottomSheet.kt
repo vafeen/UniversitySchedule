42b3a782d2a5fb6ffce60bbb0a8e5b5ada5335ac
Vafeen <666av6@gmail.com>
2024-08-23 20:12:01 +0300

    Modify BottomSheet

--- app/src/main/java/ru/vafeen/universityschedule/ui/components/bottom_sheet/UpdaterBottomSheet.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/components/bottom_sheet/UpdaterBottomSheet.kt
@@ -1,8 +1,13 @@
 package ru.vafeen.universityschedule.ui.components.bottom_sheet
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -11,24 +16,60 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import ru.vafeen.universityschedule.R
+import ru.vafeen.universityschedule.ui.theme.FontSize
+import ru.vafeen.universityschedule.ui.theme.updateAvailableColor
+import ru.vafeen.universityschedule.utils.Link
+import ru.vafeen.universityschedule.utils.openLink
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UpdaterBottomSheet(state: SheetState, onDismissRequest: () -> Unit) {
+    val context = LocalContext.current
     ModalBottomSheet(
         sheetState = state,
-        onDismissRequest = onDismissRequest
+        onDismissRequest = onDismissRequest,
+        containerColor = updateAvailableColor,
     ) {
         Column(
-            modifier = Modifier.size(600.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxSize(0.8f)
+                .padding(10.dp),
         ) {
-            Text(text = "close", modifier = Modifier.clickable {
-                onDismissRequest()
-            })
+            Text(
+                text = ":)",
+                fontSize = FontSize.gigant,
+                modifier = Modifier.align(Alignment.Start),
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = stringResource(id = R.string.update_need),
+                fontSize = FontSize.huge27, color = Color.White
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                text = stringResource(id = R.string.view_releases),
+                fontSize = FontSize.huge27, color = Color.White
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Image(
+                painter = painterResource(id = R.drawable.releases_win),
+                contentDescription = "qr",
+                modifier = Modifier
+                    .clickable {
+                        openLink(context = context, link = Link.RELEASES)
+                    }
+                    .size(150.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
--- app/src/main/java/ru/vafeen/universityschedule/ui/theme/Color.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/theme/Color.kt
@@ -4,3 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val mainLightColor = Color(0xFF68BD45)
 val mainDarkColor = Color(0xFF161617)
+val updateAvailableColor = Color(0xFF0079d6)
