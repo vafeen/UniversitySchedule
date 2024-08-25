943c541da534a6b4e3dc3f40a72395e067a3f779
A <666av6@gmail.com>
2024-08-25 15:07:49 +0300

    Fix url's data for services

---
+++ app/src/main/java/ru/vafeen/universityschedule/network/end_points/DownloadServiceLink.kt
@@ -0,0 +1,5 @@
+package ru.vafeen.universityschedule.network.end_points
+
+object DownloadServiceLink {
+    const val BASE_LINK = "https://github.com/"
+}
---
+++ app/src/main/java/ru/vafeen/universityschedule/network/end_points/GSServiceLink.kt
@@ -0,0 +1,8 @@
+package ru.vafeen.universityschedule.network.end_points
+
+object GSServiceLink {
+
+    object EndPoint {
+        const val JSON = "gviz/tq?tqx=out:json"
+    }
+}
