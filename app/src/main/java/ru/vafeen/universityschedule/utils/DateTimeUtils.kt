9bd4d8d504f7bcae5cade6a3700cfb7937715023
A <666av6@gmail.com>
2024-08-27 17:13:28 +0300

    Fix frequency calculation

--- app/src/main/java/ru/vafeen/universityschedule/utils/DateTimeUtils.kt
+++ app/src/main/java/ru/vafeen/universityschedule/utils/DateTimeUtils.kt
@@ -1,8 +1,12 @@
 package ru.vafeen.universityschedule.utils
 
+import ru.vafeen.universityschedule.noui.lesson_additions.Frequency
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.Month
+import java.time.temporal.ChronoField
 
 operator fun LocalTime.compareTo(localDateTime: LocalDateTime): Int {
     return this.compareTo(LocalTime.of(localDateTime.hour, localDateTime.minute))
@@ -16,7 +20,6 @@ fun LocalDate.getDateString(ruDaysOfWeek: List<String>): String =
     else month.value
 
 
-
 fun LocalTime.getTimeStringAsHMS(): String =
     "${
         if (hour < 10) "0$hour" else "$hour"
@@ -26,5 +29,19 @@ fun LocalTime.getTimeStringAsHMS(): String =
         if (second < 10) "0$second" else "$second"
     }"
 
+fun LocalDate.getLastSeptemberOfThisAcademicYear(): LocalDate {
+    return if (this >= LocalDate.of(year, Month.SEPTEMBER, 1))
+        LocalDate.of(year, Month.SEPTEMBER, 1)
+    else LocalDate.of(year - 1, Month.SEPTEMBER, 1)
+}
 
+fun LocalDate.getFrequencyByLocalDate(): Frequency {
+    val lastSeptember = getLastSeptemberOfThisAcademicYear()
+    val resultFrequency =
+        if (get(ChronoField.ALIGNED_WEEK_OF_YEAR) % 2 == 0) Frequency.Denominator
+        else Frequency.Numerator
+    return if (lastSeptember.dayOfWeek == DayOfWeek.SUNDAY)
+        resultFrequency.getOpposite()
+    else resultFrequency
+}
 
