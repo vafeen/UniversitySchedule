9d8bd51c8724f32168144896fce6b3203952e4c7
Vafeen <666av6@gmail.com>
2024-09-08 14:57:59 +0300

    Change icon

--- app/src/main/java/ru/vafeen/universityschedule/ui/components/screens/SettingsScreen.kt
+++ app/src/main/java/ru/vafeen/universityschedule/ui/components/screens/SettingsScreen.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -153,13 +152,13 @@ fun SettingsScreen(
             })
         },
         bottomBar = {
-        BottomBar(
-            containerColor = settings.getMainColorForThisTheme(isDark = dark)
-                ?: ScheduleTheme.colors.mainColor,
-            clickToScreen1 = gotoMainScreenCallBack,
-            selected2 = true
-        )
-    }) { innerPadding ->
+            BottomBar(
+                containerColor = settings.getMainColorForThisTheme(isDark = dark)
+                    ?: ScheduleTheme.colors.mainColor,
+                clickToScreen1 = gotoMainScreenCallBack,
+                selected2 = true
+            )
+        }) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -217,7 +216,7 @@ fun SettingsScreen(
                             text = stringResource(R.string.link_to_table),
                         )
                         Icon(
-                            imageVector = Icons.Default.Edit,
+                            imageVector = Icons.Filled.Link,
                             contentDescription = "edit link",
                             tint = ScheduleTheme.colors.oppositeTheme
                         )
