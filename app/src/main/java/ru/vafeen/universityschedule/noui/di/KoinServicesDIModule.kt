22181a969bbd5cb6a78455a22264aff18224028b
A <666av6@gmail.com>
2024-10-06 19:37:05 +0300

    Replace SharedPreferences.kt

--- app/src/main/java/ru/vafeen/universityschedule/noui/di/KoinServicesDIModule.kt
+++ app/src/main/java/ru/vafeen/universityschedule/noui/di/KoinServicesDIModule.kt
@@ -1,15 +1,22 @@
 package ru.vafeen.universityschedule.noui.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ru.vafeen.universityschedule.database.DTConverters
 import ru.vafeen.universityschedule.noui.notifications.NotificationService
 import ru.vafeen.universityschedule.noui.planner.Scheduler
-import ru.vafeen.universityschedule.noui.shared_preferences.SharedPreferences
+import ru.vafeen.universityschedule.noui.shared_preferences.SharedPreferencesValue
 
 val koinDIModule = module {
     singleOf(::NotificationService)
     singleOf(::Scheduler)
     singleOf(::DTConverters)
-    singleOf(::SharedPreferences)
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences(
+            SharedPreferencesValue.Name.key, Context.MODE_PRIVATE
+        )
+    }
 }
