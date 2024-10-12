4c1e7373888d0fbdd403225a1163e158bafcb2ef
Vafeen <666av6@gmail.com>
2024-10-12 22:51:01 +0300

    Add MyDuration to Entity Reminder and migration 4 to 5

--- app/src/main/java/ru/vafeen/universityschedule/noui/di/KoinDatabaseDIModule.kt
+++ app/src/main/java/ru/vafeen/universityschedule/noui/di/KoinDatabaseDIModule.kt
@@ -16,7 +16,8 @@ val koinDatabaseDIModule = module {
         ).addMigrations(
             AppDatabase.MIGRATION_1_2,
             AppDatabase.MIGRATION_2_3,
-            AppDatabase.MIGRATION_3_4
+            AppDatabase.MIGRATION_3_4,
+            AppDatabase.MIGRATION_4_5,
         ).build()
     }
     singleOf(::DatabaseRepository)
