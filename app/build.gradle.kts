d27ab8693c74b3474604b9cc6295183624888052
Vafeen <666av6@gmail.com>
2024-10-12 23:42:34 +0300

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ru.vafeen.universityschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "3.5"
+        versionCode = 22
+        versionName = "3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
