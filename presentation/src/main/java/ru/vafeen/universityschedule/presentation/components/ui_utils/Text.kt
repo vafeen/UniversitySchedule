66bb055bc56c00447709fc6d8162d34739810508
Vafeen <666av6@gmail.com>
2024-10-18 12:38:43 +0300

    Modify card with date layout

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/ui_utils/Text.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/ui_utils/Text.kt
@@ -1,7 +1,9 @@
 package ru.vafeen.universityschedule.presentation.components.ui_utils
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import ru.vafeen.universityschedule.presentation.theme.Theme
 
@@ -12,12 +14,14 @@ internal fun TextForThisTheme(
     text: String,
     modifier: Modifier = Modifier,
     fontSize: TextUnit = TextUnit.Unspecified,
+    textAlign: TextAlign? = null,
 ) {
-    androidx.compose.material3.Text(
+    Text(
         text = text,
         modifier = modifier,
         color = Theme.colors.oppositeTheme,
         fontSize = fontSize,
+        textAlign = textAlign,
     )
 }
 
@@ -27,7 +31,7 @@ internal fun TextForOppositeTheme(
     modifier: Modifier = Modifier,
     fontSize: TextUnit = TextUnit.Unspecified,
 ) {
-    androidx.compose.material3.Text(
+    Text(
         text = text,
         modifier = modifier,
         color = Theme.colors.singleTheme,
