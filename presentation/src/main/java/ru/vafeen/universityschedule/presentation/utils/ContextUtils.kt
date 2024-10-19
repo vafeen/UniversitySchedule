ca49981e34398913caff2a44cc677b3a11cecdd8
Vafeen <666av6@gmail.com>
2024-10-19 14:53:44 +0300

    Add copying error to clipboard

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/ContextUtils.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/ContextUtils.kt
@@ -1,6 +1,13 @@
 package ru.vafeen.universityschedule.presentation.utils
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 
 fun Context.pathToDownloadRelease(): String = "${externalCacheDir?.absolutePath}/app-release.apk"
 
+fun Context.copyToClipboard(throwable: Throwable) {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clip = ClipData.newPlainText("Error", throwable.stackTraceToString())
+    clipboard.setPrimaryClip(clip)
+}
