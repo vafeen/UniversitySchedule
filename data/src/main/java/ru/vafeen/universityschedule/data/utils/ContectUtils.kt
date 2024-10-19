bc5f72eb7e21d1e925413e5b15dd334739cacb22
A <666av6@gmail.com>
2024-10-19 19:26:52 +0300

    Modify copyTextToClipBoard

--- data/src/main/java/ru/vafeen/universityschedule/data/utils/ContectUtils.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/utils/ContectUtils.kt
@@ -4,9 +4,9 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 
-fun Context.copyTextToClipBoard(text: String) {
+fun Context.copyTextToClipBoard(label: String, text: String) {
     val clipboard =
         getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clip = ClipData.newPlainText("label", text)
+    val clip = ClipData.newPlainText(label, text)
     if (text.isNotEmpty()) clipboard.setPrimaryClip(clip)
 }
