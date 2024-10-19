cdb695cb0c6d360b2107ee0749879b6a3fb4676a
A <666av6@gmail.com>
2024-10-19 19:32:41 +0300

    Move exception handler to viewModel

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/MainActivity.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.vafeen.universityschedule.presentation
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,7 +13,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.vafeen.universityschedule.data.utils.copyTextToClipBoard
 import ru.vafeen.universityschedule.presentation.components.permissions.RequestNotificationPermission
 import ru.vafeen.universityschedule.presentation.components.screens.MainScreen
 import ru.vafeen.universityschedule.presentation.components.screens.SettingsScreen
@@ -23,8 +21,6 @@ import ru.vafeen.universityschedule.presentation.components.viewModels.MainActiv
 import ru.vafeen.universityschedule.presentation.navigation.Screen
 import ru.vafeen.universityschedule.presentation.theme.MainTheme
 import ru.vafeen.universityschedule.presentation.theme.Theme
-import ru.vafeen.universityschedule.presentation.utils.Link
-import kotlin.system.exitProcess
 
 
 class MainActivity : ComponentActivity() {
@@ -35,14 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            copyTextToClipBoard(
-                label = "Error",
-                text = "Contact us about this problem: ${Link.MAIL}\n\n Exception in ${thread.name} thread\n${throwable.stackTraceToString()}"
-            )
-            Log.e("UncaughtException", "Uncaught exception in thread ${thread.name}", throwable)
-            exitProcess(0)
-        }
+        viewModel.registerGeneralExceptionCallback(context = this)
         setContent {
             RequestNotificationPermission()
             MainTheme {
