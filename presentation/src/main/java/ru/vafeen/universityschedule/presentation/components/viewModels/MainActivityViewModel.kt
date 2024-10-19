f19e75d41bb92078ff30d7e5e82f7301211354bc
Vafeen <666av6@gmail.com>
2024-10-19 19:32:41 +0300

    Move exception handler to viewModel

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/viewModels/MainActivityViewModel.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/viewModels/MainActivityViewModel.kt
@@ -1,11 +1,26 @@
 package ru.vafeen.universityschedule.presentation.components.viewModels
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import ru.vafeen.universityschedule.data.network.repository.NetworkRepository
+import ru.vafeen.universityschedule.data.utils.copyTextToClipBoard
+import ru.vafeen.universityschedule.presentation.utils.Link
+import kotlin.system.exitProcess
 
 
 internal class MainActivityViewModel(
     val networkRepository: NetworkRepository,
 ) : ViewModel() {
     var updateIsShowed = false
+    fun registerGeneralExceptionCallback(context: Context) {
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            context.copyTextToClipBoard(
+                label = "Error",
+                text = "Contact us about this problem: ${Link.MAIL}\n\n Exception in ${thread.name} thread\n${throwable.stackTraceToString()}"
+            )
+            Log.e("GeneralException", "Exception in thread ${thread.name}", throwable)
+            exitProcess(0)
+        }
+    }
 }
