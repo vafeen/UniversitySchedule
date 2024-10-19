e169205b97357f0aa2c4bd23461a9d575bd5d5ca
A <666av6@gmail.com>
2024-10-19 19:22:32 +0300

    Add SerializedName to some variables

--- data/src/main/java/ru/vafeen/universityschedule/data/network/parcelable/github_service/Asset.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/network/parcelable/github_service/Asset.kt
@@ -1,17 +1,19 @@
 package ru.vafeen.universityschedule.data.network.parcelable.github_service
 
+import com.google.gson.annotations.SerializedName
+
 data class Asset(
     val url: String,
     val id: Long,
-    val node_id: String,
+    @SerializedName("node_id") val nodeId: String,
     val name: String,
     val label: String?,
     val uploader: Author,
-    val content_type: String,
+    @SerializedName("content_type") val contentType: String,
     val state: String,
     val size: Long,
-    val download_count: Int,
-    val created_at: String,
-    val updated_at: String,
-    val browser_download_url: String
+    @SerializedName("download_count") val downloadCount: Int,
+    @SerializedName("created_at") val createdAt: String,
+    @SerializedName("updated_at") val updatedAt: String,
+    @SerializedName("browser_download_url") val browserDownloadUrl: String
 )
