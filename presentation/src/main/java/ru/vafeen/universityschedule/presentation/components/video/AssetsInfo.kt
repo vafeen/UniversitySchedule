e820e49984b848eb61acb4ba896821c9e6d5e7c0
A <666av6@gmail.com>
2024-10-22 03:09:53 +0300

    Add dancing cat to weekday days

---
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/video/AssetsInfo.kt
@@ -0,0 +1,6 @@
+package ru.vafeen.universityschedule.presentation.components.video
+
+object AssetsInfo {
+    private const val PREFIX = "file:///android_asset"
+    const val DANCING_CAT = "$PREFIX/dancing_cat.gif"
+}
---
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/video/videoplayer.kt
@@ -0,0 +1,30 @@
+package ru.vafeen.universityschedule.presentation.components.video
+
+import android.net.Uri
+import android.widget.ImageView
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.bumptech.glide.Glide
+
+@Composable
+fun GifPlayer(
+    size: Dp,
+    modifier: Modifier = Modifier,
+    imageUri: Uri
+) {
+    AndroidView(
+        modifier = modifier.size(size),
+        factory = { context ->
+            ImageView(context).apply {
+                // Используем Glide для загрузки изображения из Uri
+                Glide.with(context)
+                    .asGif() // Указываем, что это GIF
+                    .load(imageUri)
+                    .into(this)
+            }
+        })
+}
