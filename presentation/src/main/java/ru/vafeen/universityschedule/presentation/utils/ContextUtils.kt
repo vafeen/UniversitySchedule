7e8502058708c4e0aac356e13115b088e830e3d3
Vafeen <666av6@gmail.com>
2024-10-27 02:28:03 +0300

    Modify BugReport string

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/ContextUtils.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/ContextUtils.kt
@@ -3,12 +3,12 @@ package ru.vafeen.universityschedule.presentation.utils
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-
+import  ru.vafeen.universityschedule.data.R
 
 internal fun Context.sendEmail(email: String) {
     val intent = Intent(Intent.ACTION_SENDTO).apply {
         data =
-            Uri.parse("mailto:$email?subject=${Uri.encode("BugReport")}")
+            Uri.parse("mailto:$email?subject=${Uri.encode("BugReport ${getString(R.string.app_name)}")}")
     }
     startActivity(intent)
 }
