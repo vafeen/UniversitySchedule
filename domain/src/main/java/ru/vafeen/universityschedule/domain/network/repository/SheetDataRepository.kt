7e627ed74b21362e14a2fd8eae08f6e757420dca
A <666av6@gmail.com>
2024-11-04 16:52:08 +0300

    Rename GoogleSheetsRepository to SheetDataRepository

--- domain/src/main/java/ru/vafeen/universityschedule/domain/network/repository/SheetDataRepository.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/network/repository/SheetDataRepository.kt
@@ -3,6 +3,6 @@ package ru.vafeen.universityschedule.domain.network.repository
 import retrofit2.http.Url
 import ru.vafeen.universityschedule.domain.models.Lesson
 
-interface GoogleSheetsRepository {
+interface SheetDataRepository {
     suspend fun getLessonsListFromGSheetsTable(@Url link: String): List<Lesson>?
 }
--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataUseCase.kt
@@ -2,12 +2,12 @@ package ru.vafeen.universityschedule.domain.usecase.network
 
 import ru.vafeen.universityschedule.domain.models.Lesson
 import ru.vafeen.universityschedule.domain.network.end_points.GoogleSheetsServiceLink
-import ru.vafeen.universityschedule.domain.network.repository.GoogleSheetsRepository
+import ru.vafeen.universityschedule.domain.network.repository.SheetDataRepository
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
-class GetSheetDataUseCase(private val googleSheetsRepository: GoogleSheetsRepository) : UseCase {
+class GetSheetDataUseCase(private val sheetDataRepository: SheetDataRepository) : UseCase {
     suspend fun use(link: String): List<Lesson>? =
-        googleSheetsRepository.getLessonsListFromGSheetsTable(
+        sheetDataRepository.getLessonsListFromGSheetsTable(
             "${
                 link.substringBefore("/edit?")
                     .substringAfter(GoogleSheetsServiceLink.BASE_URL)
