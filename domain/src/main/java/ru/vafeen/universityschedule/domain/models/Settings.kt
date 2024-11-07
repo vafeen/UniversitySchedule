c6ecb371a7a6139e4ff6db8a303d2ba97663f744
A <666av6@gmail.com>
2024-11-07 06:46:44 +0300

    Modify settings defaults

--- domain/src/main/java/ru/vafeen/universityschedule/domain/models/Settings.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/models/Settings.kt
@@ -12,6 +12,8 @@ data class Settings(
     val lastDemonstratedVersion: Int? = null,
     val weekendCat: Boolean = false,
     val catInSettings: Boolean = false,
+    val notesAboutLesson: Boolean = false,
+    val notificationsAboutLesson: Boolean = true,
 ) {
     fun toJsonString(): String = Gson().toJson(this)
 }
