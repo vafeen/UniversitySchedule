e9287215172d02338e13efe114932a988f748ac9
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/InsertLessonsUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/InsertLessonsUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/InsertLessonsUseCase.kt
@@ -4,6 +4,19 @@ import ru.vafeen.universityschedule.domain.database.LessonRepository
 import ru.vafeen.universityschedule.domain.models.Lesson
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для добавления пар в базу данных.
+ *
+ * Этот класс отвечает за вставку одного или нескольких уроков в репозиторий.
+ *
+ * @property lessonRepository Репозиторий, используемый для взаимодействия с данными пар.
+ */
 class InsertLessonsUseCase(private val lessonRepository: LessonRepository) : UseCase {
+
+    /**
+     * Вставляет указанные пары в базу данных.
+     *
+     * @param lesson Пары, которые нужно добавить. Можно передавать несколько пар в виде vararg.
+     */
     suspend fun invoke(vararg lesson: Lesson) = lessonRepository.insertLessons(lesson.toList())
 }
