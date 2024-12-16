021139c3094e9c8198a38879473154d9bae617c4
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetAsFlowRemindersUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetAsFlowRemindersUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetAsFlowRemindersUseCase.kt
@@ -5,6 +5,20 @@ import ru.vafeen.universityschedule.domain.database.ReminderRepository
 import ru.vafeen.universityschedule.domain.models.Reminder
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для получения списка напоминаний в виде потока.
+ *
+ * Этот класс предоставляет возможность получать текущий список напоминаний из репозитория в виде
+ * потока, что позволяет наблюдать за изменениями в данных.
+ *
+ * @property reminderRepository Репозиторий, используемый для взаимодействия с данными напоминаний.
+ */
 class GetAsFlowRemindersUseCase(private val reminderRepository: ReminderRepository) : UseCase {
+
+    /**
+     * Возвращает поток, содержащий текущий список напоминаний.
+     *
+     * @return [Flow] с итерацией напоминаний [Reminder].
+     */
     fun invoke(): Flow<Iterable<Reminder>> = reminderRepository.getAsFlowReminders()
 }
