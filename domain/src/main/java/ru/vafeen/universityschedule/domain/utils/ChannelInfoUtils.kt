82b718daee3e156de0085972e420cf6265b52000
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/utils/ChannelInfoUtils.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/utils/ChannelInfoUtils.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/utils/ChannelInfoUtils.kt
@@ -4,7 +4,13 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import ru.vafeen.universityschedule.domain.notifications.ChannelInfo
 
-
+/**
+ * Расширение для класса [ChannelInfo], позволяющее создать объект [NotificationChannel].
+ *
+ * Эта функция создает новый канал уведомлений на основе информации, содержащейся в объекте [ChannelInfo].
+ *
+ * @return Новый экземпляр [NotificationChannel], настроенный с использованием данных из [ChannelInfo].
+ */
 fun ChannelInfo.createNotificationChannelKClass(): NotificationChannel =
     NotificationChannel(
         notificationChannelID,
