2b45a76df6c0cce4d3b8d32f7f3b86936814941d
Vafeen <666av6@gmail.com>
2024-12-16 04:11:11 +0300

    add docs to data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/TableDTO.kt

--- data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/TableDTO.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/TableDTO.kt
@@ -2,10 +2,17 @@ package ru.vafeen.universityschedule.data.network.dto.googlesheets_service
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Класс, представляющий таблицу из данных Google Sheets.
+ *
+ * @property cols Список столбцов таблицы.
+ * @property rows Список строк таблицы.
+ * @property parsedNumHeaders Количество заголовков, распознанных в таблице.
+ */
 internal data class TableDTO(
-    @SerializedName("cols") val cols: List<ColumnDTO>,
-    @SerializedName("rows") val rows: List<RowDTO>,
-    @SerializedName("parsedNumHeaders") val parsedNumHeaders: Int
+    @SerializedName("cols") val cols: List<ColumnDTO>, // Список столбцов таблицы
+    @SerializedName("rows") val rows: List<RowDTO>, // Список строк таблицы
+    @SerializedName("parsedNumHeaders") val parsedNumHeaders: Int // Количество заголовков
 ) {
-    override fun toString(): String = rows.toString()
+    override fun toString(): String = rows.toString() // Переопределение метода toString для удобства отображения данных строк
 }
