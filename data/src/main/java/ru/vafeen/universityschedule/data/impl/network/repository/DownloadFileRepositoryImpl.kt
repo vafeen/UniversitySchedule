0e5d34dda7855d2d15e6d4ecf191090a51aa3608
Vafeen <666av6@gmail.com>
2024-12-16 04:11:11 +0300

    add docs to data/src/main/java/ru/vafeen/universityschedule/data/impl/network/repository/DownloadFileRepositoryImpl.kt

--- data/src/main/java/ru/vafeen/universityschedule/data/impl/network/repository/DownloadFileRepositoryImpl.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/impl/network/repository/DownloadFileRepositoryImpl.kt
@@ -7,15 +7,27 @@ import ru.vafeen.universityschedule.domain.network.service.DownloadFileRepositor
 import java.io.IOException
 import java.net.UnknownHostException
 
+/**
+ * Реализация репозитория для загрузки файлов через сеть.
+ *
+ * @property downloadService Сервис для выполнения запросов на загрузку файлов.
+ */
 internal class DownloadFileRepositoryImpl(
     private val downloadService: DownloadService
 ) : DownloadFileRepository {
 
+    /**
+     * Загрузка файла по указанному URL.
+     *
+     * @param fileUrl URL файла для загрузки.
+     * @return Результат загрузки файла, содержащий поток данных или ошибку.
+     */
     override suspend fun downloadFile(fileUrl: String): ResponseResult<CompositeFileStream> = try {
         // Выполнение запроса
         val call = downloadService.downloadFile(fileUrl)
         val response = call.execute() // Синхронный вызов
         val body = response.body()
+
         // Проверяем успешность ответа и наличие тела
         if (response.isSuccessful && body != null) {
             val contentLength = body.contentLength()
@@ -41,6 +53,3 @@ internal class DownloadFileRepositoryImpl(
         ResponseResult.Error(Exception("Неизвестная ошибка: ${e.localizedMessage}"))
     }
 }
-
-
-
