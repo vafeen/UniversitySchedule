8baa615f751bc98e4013888cbbc01a99816efbd5
Vafeen <666av6@gmail.com>
2024-12-16 04:11:11 +0300

    add docs to data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/CellDTO.kt

--- data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/CellDTO.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/network/dto/googlesheets_service/CellDTO.kt
@@ -2,8 +2,13 @@ package ru.vafeen.universityschedule.data.network.dto.googlesheets_service
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Класс, представляющий ячейку таблицы из данных Google Sheets.
+ *
+ * @property value Значение, содержащееся в ячейке. Может быть null, если ячейка пуста.
+ */
 internal data class CellDTO(
-    @SerializedName("v") val value: String?
+    @SerializedName("v") val value: String? // Значение ячейки
 ) {
-    override fun toString(): String = value.toString()
+    override fun toString(): String = value.toString() // Переопределение метода toString для удобства отображения значения ячейки
 }
