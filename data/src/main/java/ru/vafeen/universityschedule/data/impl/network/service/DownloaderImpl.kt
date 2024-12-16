aede6b68a101c310c2709e8c287ace7af8dbf200
Vafeen <666av6@gmail.com>
2024-12-16 04:11:11 +0300

    add docs to data/src/main/java/ru/vafeen/universityschedule/data/impl/network/service/DownloaderImpl.kt

--- data/src/main/java/ru/vafeen/universityschedule/data/impl/network/service/DownloaderImpl.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/impl/network/service/DownloaderImpl.kt
@@ -14,46 +14,67 @@ import ru.vafeen.universityschedule.domain.utils.pathToDownloadRelease
 import java.io.File
 import java.io.FileOutputStream
 
+/**
+ * Реализация загрузчика для скачивания APK файлов.
+ *
+ * @property context Контекст приложения.
+ * @property downloadFileRepository Репозиторий для загрузки файлов.
+ * @property installer Установщик для установки загруженного APK.
+ */
 internal class DownloaderImpl(
     private val context: Context,
     private val downloadFileRepository: DownloadFileRepository,
     private val installer: Installer,
 ) : Downloader {
+
+    /**
+     * Поток для отслеживания процента загрузки.
+     */
     private val _percentageFlow = MutableSharedFlow<Float>()
     override val percentageFlow = _percentageFlow.asSharedFlow()
 
+    /**
+     * Поток для отслеживания статуса процесса обновления.
+     */
     private val _isUpdateInProcessFlow = MutableSharedFlow<Boolean>()
     override val isUpdateInProcessFlow = _isUpdateInProcessFlow.asSharedFlow()
 
+    /**
+     * Загрузка APK файла по указанному URL.
+     *
+     * @param url URL для загрузки APK файла.
+     */
     override fun downloadApk(url: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            _isUpdateInProcessFlow.emit(true)
-            val apkFilePath = context.pathToDownloadRelease()
+            _isUpdateInProcessFlow.emit(true) // Устанавливаем статус обновления в процессе
+            val apkFilePath = context.pathToDownloadRelease() // Получаем путь для сохранения APK
+
             when (val result = downloadFileRepository.downloadFile(url)) {
                 is ResponseResult.Error -> {
-                    _isUpdateInProcessFlow.emit(false)
-                    _percentageFlow.emit(0f)
+                    _isUpdateInProcessFlow.emit(false) // Обновление завершено с ошибкой
+                    _percentageFlow.emit(0f) // Процент загрузки 0%
                 }
 
                 is ResponseResult.Success -> {
-                    val file = File(apkFilePath)
-                    val inputStream = result.data.stream
-                    val outputStream = FileOutputStream(file)
-                    val buffer = ByteArray(8 * 1024)
-                    var bytesRead: Int
-                    var totalBytesRead: Long = 0
-                    val contentLength = result.data.contentLength
+                    val file = File(apkFilePath) // Создаем объект файла для сохранения APK
+                    val inputStream = result.data.stream // Получаем InputStream из результата загрузки
+                    val outputStream = FileOutputStream(file) // Создаем поток для записи в файл
+                    val buffer = ByteArray(8 * 1024) // Буфер для чтения данных (8 КБ)
+                    var bytesRead: Int // Переменная для хранения количества прочитанных байтов
+                    var totalBytesRead: Long = 0 // Общая длина загруженных байтов
+                    val contentLength = result.data.contentLength // Общая длина загружаемого контента
 
+                    // Чтение данных из InputStream и запись в файл
                     while (inputStream.read(buffer).also { bytesRead = it } != -1) {
                         outputStream.write(buffer, 0, bytesRead)
                         totalBytesRead += bytesRead
-                        _percentageFlow.emit(totalBytesRead.toFloat() / contentLength)
+                        _percentageFlow.emit(totalBytesRead.toFloat() / contentLength) // Обновляем процент загрузки
                     }
 
-                    outputStream.close()
-                    inputStream.close()
-                    _isUpdateInProcessFlow.emit(false)
-                    installer.installApk()
+                    outputStream.close() // Закрываем поток записи
+                    inputStream.close() // Закрываем поток чтения
+                    _isUpdateInProcessFlow.emit(false) // Обновление завершено успешно
+                    installer.installApk() // Устанавливаем загруженный APK
                 }
             }
         }
