d46a0bb3cd9788066999cb7c21dda6b1a61f4abe
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetReminderByIdOfReminderUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetReminderByIdOfReminderUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/db/GetReminderByIdOfReminderUseCase.kt
@@ -4,7 +4,22 @@ import ru.vafeen.universityschedule.domain.database.ReminderRepository
 import ru.vafeen.universityschedule.domain.models.Reminder
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для получения напоминания по его идентификатору.
+ *
+ * Этот класс предоставляет возможность получать конкретное напоминание из репозитория
+ * по его идентификатору.
+ *
+ * @property reminderRepository Репозиторий, используемый для взаимодействия с данными напоминаний.
+ */
 class GetReminderByIdOfReminderUseCase(private val reminderRepository: ReminderRepository) : UseCase {
+
+    /**
+     * Возвращает напоминание по указанному идентификатору.
+     *
+     * @param idOfReminder Идентификатор напоминания, которое нужно получить.
+     * @return Напоминание [Reminder] с указанным идентификатором или null, если напоминание не найдено.
+     */
     fun invoke(idOfReminder: Int): Reminder? =
         reminderRepository.getReminderByIdOfReminder(idOfReminder = idOfReminder)
 }
