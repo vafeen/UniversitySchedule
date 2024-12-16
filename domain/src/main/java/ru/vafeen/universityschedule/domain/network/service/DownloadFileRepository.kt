3d83cdd208a93372eaf846ec594a44089dc16c78
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/network/service/DownloadFileRepository.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/network/service/DownloadFileRepository.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/network/service/DownloadFileRepository.kt
@@ -1,10 +1,18 @@
 package ru.vafeen.universityschedule.domain.network.service
 
-
 import ru.vafeen.universityschedule.domain.models.CompositeFileStream
 import ru.vafeen.universityschedule.domain.network.result.ResponseResult
 
-
+/**
+ * Интерфейс репозитория для загрузки файлов.
+ */
 interface DownloadFileRepository {
+
+    /**
+     * Загружает файл по указанному URL.
+     *
+     * @param fileUrl URL файла, который нужно загрузить.
+     * @return [ResponseResult] с результатом загрузки, содержащим объект [CompositeFileStream].
+     */
     suspend fun downloadFile(fileUrl: String): ResponseResult<CompositeFileStream>
 }
