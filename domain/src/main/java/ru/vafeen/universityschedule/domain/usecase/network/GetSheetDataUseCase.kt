2a127fea1f729fc55c64d1c1fcd19223350fe7a3
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataUseCase.kt
@@ -6,7 +6,21 @@ import ru.vafeen.universityschedule.domain.network.repository.SheetDataRepositor
 import ru.vafeen.universityschedule.domain.network.result.ResponseResult
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для получения данных о парах из таблицы Google Sheets.
+ *
+ * Этот класс отвечает за выполнение операции получения списка пар из указанной таблицы Google Sheets.
+ *
+ * @property sheetDataRepository Репозиторий, используемый для взаимодействия с данными таблицы.
+ */
 class GetSheetDataUseCase(private val sheetDataRepository: SheetDataRepository) : UseCase {
+
+    /**
+     * Получает список пар из таблицы Google Sheets по указанной ссылке.
+     *
+     * @param link Ссылка на таблицу Google Sheets, откуда нужно получить данные.
+     * @return [ResponseResult] с списком пар [Lesson] или ошибкой, если произошла ошибка при получении данных.
+     */
     suspend fun invoke(link: String): ResponseResult<List<Lesson>> =
         sheetDataRepository.getLessonsListFromGSheetsTable(
             "${
@@ -14,6 +28,4 @@ class GetSheetDataUseCase(private val sheetDataRepository: SheetDataRepository) 
                     .substringAfter(GoogleSheetsServiceLink.BASE_URL)
             }/${GoogleSheetsServiceLink.EndPoint.JSON}"
         )
-
-
 }
