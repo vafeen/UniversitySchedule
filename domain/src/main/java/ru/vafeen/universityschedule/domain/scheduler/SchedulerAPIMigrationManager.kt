d9f46e8ebe4ef88f0d8bd280c30b693d30c2af32
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/scheduler/SchedulerAPIMigrationManager.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/scheduler/SchedulerAPIMigrationManager.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/scheduler/SchedulerAPIMigrationManager.kt
@@ -1,5 +1,14 @@
 package ru.vafeen.universityschedule.domain.scheduler
 
+/**
+ * Интерфейс для управления миграцией API планировщика.
+ */
 interface SchedulerAPIMigrationManager {
+
+    /**
+     * Выполняет миграцию данных или настроек для API планировщика.
+     *
+     * Этот метод должен быть вызван в корутине, так как он является приостановленным.
+     */
     suspend fun migrate()
 }
