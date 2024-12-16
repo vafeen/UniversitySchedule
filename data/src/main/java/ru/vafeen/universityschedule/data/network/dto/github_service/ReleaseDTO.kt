906b4d0a3dd9e95c291879c9e72dec90bd4226cc
Vafeen <666av6@gmail.com>
2024-12-16 04:11:11 +0300

    add docs to data/src/main/java/ru/vafeen/universityschedule/data/network/dto/github_service/ReleaseDTO.kt

--- data/src/main/java/ru/vafeen/universityschedule/data/network/dto/github_service/ReleaseDTO.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/network/dto/github_service/ReleaseDTO.kt
@@ -2,6 +2,28 @@ package ru.vafeen.universityschedule.data.network.dto.github_service
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Данные о релизе из GitHub API.
+ *
+ * @property url URL релиза.
+ * @property assetsUrl URL для получения активов (assets) релиза.
+ * @property uploadUrl URL для загрузки активов в релиз.
+ * @property htmlUrl URL релиза в формате HTML.
+ * @property id Уникальный идентификатор релиза.
+ * @property author Информация об авторе релиза.
+ * @property nodeId Идентификатор узла релиза.
+ * @property tagName Название тега, связанного с релизом.
+ * @property targetCommitish Указывает на коммит, к которому относится релиз.
+ * @property name Название релиза.
+ * @property draft Флаг, указывающий, является ли релиз черновиком.
+ * @property preRelease Флаг, указывающий, является ли релиз предварительным.
+ * @property createdAt Дата и время создания релиза.
+ * @property publishedAt Дата и время публикации релиза.
+ * @property assets Список активов, связанных с релизом.
+ * @property tarballUrl URL для загрузки tarball архива релиза.
+ * @property zipballUrl URL для загрузки zip архива релиза.
+ * @property body Описание релиза.
+ */
 internal data class ReleaseDTO(
     @SerializedName("url") val url: String,
     @SerializedName("assets_url") val assetsUrl: String,
@@ -22,4 +44,3 @@ internal data class ReleaseDTO(
     @SerializedName("zipball_url") val zipballUrl: String,
     @SerializedName("body") val body: String
 )
-
