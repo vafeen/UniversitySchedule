5028cad6a78ec3486c96826bec15165520227f46
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/models/Reminder.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/models/Reminder.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/models/Reminder.kt
@@ -4,6 +4,17 @@ import ru.vafeen.universityschedule.domain.models.model_additions.ReminderType
 import ru.vafeen.universityschedule.domain.scheduler.duration.RepeatDuration
 import java.time.LocalDateTime
 
+/**
+ * Класс, представляющий напоминание.
+ *
+ * @property id Уникальный идентификатор напоминания.
+ * @property idOfReminder Идентификатор напоминания.
+ * @property title Заголовок напоминания.
+ * @property text Текст напоминания.
+ * @property dt Дата и время, когда должно сработать напоминание.
+ * @property duration Продолжительность повторения напоминания (по умолчанию - каждую неделю).
+ * @property type Тип напоминания (по умолчанию - после начала пары).
+ */
 data class Reminder(
     val id: Int = 0,
     val idOfReminder: Int,
@@ -11,5 +22,5 @@ data class Reminder(
     val text: String,
     val dt: LocalDateTime,
     val duration: RepeatDuration = RepeatDuration.EVERY_WEEK,
-    val type: ReminderType = ReminderType.AFTER_BEGINNING_LESSON,
+    val type: ReminderType = ReminderType.AFTER_BEGINNING_LESSON
 )
