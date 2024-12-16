81722632ff827c8d6a5fd7763a18dd4417c098d6
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/FrequencyUtils.kt

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/FrequencyUtils.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/FrequencyUtils.kt
@@ -3,6 +3,14 @@ package ru.vafeen.universityschedule.presentation.utils
 import ru.vafeen.universityschedule.domain.models.Settings
 import ru.vafeen.universityschedule.domain.models.model_additions.Frequency
 
+/**
+ * Функция для изменения частоты в зависимости от настроек.
+ * Если в настройках указано, что выбранная частота соответствует номерам недель (isSelectedFrequencyCorrespondsToTheWeekNumbers),
+ * то возвращается текущая частота. В противном случае возвращается противоположная частота.
+ *
+ * @param settings Настройки, содержащие информацию о выбранной частоте.
+ * @return Возвращает текущую или противоположную частоту в зависимости от значения настроек.
+ */
 internal fun Frequency.changeFrequencyIfDefinedInSettings(settings: Settings): Frequency {
     return if (settings.isSelectedFrequencyCorrespondsToTheWeekNumbers != false)
         this
