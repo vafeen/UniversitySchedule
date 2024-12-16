c1cc6b38b50a0948f1ebcf31a2a2d09673313db7
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/DownloadFileUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/DownloadFileUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/DownloadFileUseCase.kt
@@ -6,7 +6,21 @@ import ru.vafeen.universityschedule.domain.network.result.ResponseResult
 import ru.vafeen.universityschedule.domain.network.service.DownloadFileRepository
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для загрузки файлов из сети.
+ *
+ * Этот класс отвечает за выполнение операции загрузки файла с указанного URL.
+ *
+ * @property downloadFileRepository Репозиторий, используемый для взаимодействия с сетевыми запросами на загрузку файлов.
+ */
 class DownloadFileUseCase(private val downloadFileRepository: DownloadFileRepository) : UseCase {
+
+    /**
+     * Загружает файл по указанному URL.
+     *
+     * @param fileUrl URL файла, который нужно загрузить. Аннотация [Url] указывает, что параметр должен быть корректным URL.
+     * @return [ResponseResult] с потоком загруженного файла [CompositeFileStream].
+     */
     suspend fun invoke(@Url fileUrl: String): ResponseResult<CompositeFileStream> =
         downloadFileRepository.downloadFile(fileUrl = fileUrl)
 }
