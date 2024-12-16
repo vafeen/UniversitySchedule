f62a974f2c487552ba491b749439ebdc964893f4
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataAndUpdateDBUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataAndUpdateDBUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/network/GetSheetDataAndUpdateDBUseCase.kt
@@ -5,24 +5,40 @@ import ru.vafeen.universityschedule.domain.network.result.ResponseResult
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 import ru.vafeen.universityschedule.domain.usecase.db.CleverUpdatingLessonsUseCase
 
+/**
+ * UseCase для получения данных из Google Sheets и обновления базы данных.
+ *
+ * Этот класс отвечает за выполнение операции получения данных из указанной таблицы Google Sheets
+ * и обновления базы данных на основе полученных данных.
+ *
+ * @property getSheetDataUseCase UseCase для получения данных из Google Sheets.
+ * @property cleverUpdatingLessonsUseCase UseCase для интеллектуального обновления пар.
+ */
 class GetSheetDataAndUpdateDBUseCase(
     private val getSheetDataUseCase: GetSheetDataUseCase,
     private val cleverUpdatingLessonsUseCase: CleverUpdatingLessonsUseCase,
 ) : UseCase {
+
+    /**
+     * Получает данные из Google Sheets и обновляет базу данных.
+     *
+     * @param link Ссылка на таблицу Google Sheets, откуда нужно получить данные.
+     * @param updateRequestStatus Функция обратного вызова для обновления статуса запроса (по умолчанию null).
+     */
     suspend fun invoke(
         link: String,
         updateRequestStatus: (suspend (GSheetsServiceRequestStatus) -> Unit)? = null
     ) {
-        updateRequestStatus?.invoke(GSheetsServiceRequestStatus.Waiting)
+        updateRequestStatus?.invoke(GSheetsServiceRequestStatus.Waiting) // Устанавливаем статус ожидания
         getSheetDataUseCase.invoke(link).also {
             when (it) {
                 is ResponseResult.Error -> {
-                    updateRequestStatus?.invoke(GSheetsServiceRequestStatus.NetworkError)
+                    updateRequestStatus?.invoke(GSheetsServiceRequestStatus.NetworkError) // Обработка ошибки сети
                 }
 
                 is ResponseResult.Success -> {
-                    cleverUpdatingLessonsUseCase.invoke(it.data)
-                    updateRequestStatus?.invoke(GSheetsServiceRequestStatus.Success)
+                    cleverUpdatingLessonsUseCase.invoke(it.data) // Обновление базы данных с полученными данными
+                    updateRequestStatus?.invoke(GSheetsServiceRequestStatus.Success) // Устанавливаем статус успеха
                 }
             }
         }
