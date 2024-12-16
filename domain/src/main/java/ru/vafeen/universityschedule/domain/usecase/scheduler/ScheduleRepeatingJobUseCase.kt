2306049b8618c242b6c1e4c4e8e490aa15328f03
Vafeen <666av6@gmail.com>
2024-12-16 04:11:12 +0300

    add docs to domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/scheduler/ScheduleRepeatingJobUseCase.kt

--- domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/scheduler/ScheduleRepeatingJobUseCase.kt
+++ domain/src/main/java/ru/vafeen/universityschedule/domain/usecase/scheduler/ScheduleRepeatingJobUseCase.kt
@@ -4,8 +4,21 @@ import ru.vafeen.universityschedule.domain.models.Reminder
 import ru.vafeen.universityschedule.domain.scheduler.Scheduler
 import ru.vafeen.universityschedule.domain.usecase.base.UseCase
 
+/**
+ * UseCase для планирования повторяющейся задачи.
+ *
+ * Этот класс отвечает за планирование задачи, связанной с указанным напоминанием.
+ *
+ * @property scheduler Планировщик, используемый для управления задачами (например, планирования повторяющихся напоминаний).
+ */
 class ScheduleRepeatingJobUseCase(private val scheduler: Scheduler) : UseCase {
+
+    /**
+     * Планирует повторяющуюся задачу для указанного напоминания.
+     *
+     * @param reminder Напоминание, для которого нужно запланировать повторяющуюся задачу.
+     */
     fun invoke(reminder: Reminder) {
-        scheduler.scheduleRepeatingJob(reminder = reminder)
+        scheduler.scheduleRepeatingJob(reminder = reminder) // Планирование задачи в планировщике.
     }
 }
