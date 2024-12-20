b65b6c9bba29e90671bcd70b4e0d0175663154ee
Vafeen <666av6@gmail.com>
2024-12-20 19:23:06 +0300

    Change updater API

--- data/src/main/java/ru/vafeen/universityschedule/data/di/Modules.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/di/Modules.kt
@@ -3,6 +3,7 @@ package ru.vafeen.universityschedule.data.di
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ru.vafeen.universityschedule.data.converters.DateTimeConverter
+import ru.vafeen.universityschedule.data.converters.DownloadStatusConverter
 import ru.vafeen.universityschedule.data.converters.LessonConverter
 import ru.vafeen.universityschedule.data.converters.ReleaseConverter
 import ru.vafeen.universityschedule.data.converters.ReminderConverter
@@ -10,6 +11,7 @@ import ru.vafeen.universityschedule.data.converters.TimeConverter
 
 internal val converterModule = module {
     singleOf(::DateTimeConverter)
+    singleOf(::DownloadStatusConverter)
     singleOf(::LessonConverter)
     singleOf(::ReleaseConverter)
     singleOf(::ReminderConverter)
