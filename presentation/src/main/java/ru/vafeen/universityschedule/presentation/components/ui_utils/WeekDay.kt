8542c43eea4ae050af886de4431d61715f401db5
A <666av6@gmail.com>
2024-12-24 01:05:34 +0300

    Fix:weekend cat position (#26)
    * Move cat to WeekDay component
    
    * Add isCatShowed for showing cat

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/ui_utils/WeekDay.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/components/ui_utils/WeekDay.kt
@@ -1,27 +1,44 @@
 package ru.vafeen.universityschedule.presentation.components.ui_utils
 
 import android.content.Context
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.vafeen.universityschedule.presentation.components.video.AssetsInfo
+import ru.vafeen.universityschedule.presentation.components.video.GifPlayer
+import ru.vafeen.universityschedule.presentation.components.viewModels.MainScreenViewModel
 import ru.vafeen.universityschedule.presentation.theme.FontSize
 import ru.vafeen.universityschedule.resources.R
 
 @Composable
-internal fun WeekDay(context: Context, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+internal fun WeekDay(
+    isCatShowed: Boolean,
+    context: Context, modifier: Modifier = Modifier, viewModel: MainScreenViewModel
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
         TextForThisTheme(
             text = context.getString(R.string.holiday),
             fontSize = FontSize.huge27,
             modifier = Modifier
         )
+        // Отображение GIF-картинки, если нет пар и выходной день.
+        if (isCatShowed) {
+            GifPlayer(
+                size = 150.dp,
+                modifier = Modifier
+                    .clickable { viewModel.meow() },
+                imageUri = Uri.parse(AssetsInfo.DANCING_CAT)
+            )
+        }
     }
 }
