670ff268f8b68a146d4ac24f10a965b524dc8ace
Vafeen <666av6@gmail.com>
2025-02-07 18:48:48 +0300

    New version: 6.1.19

--- CHANGE.md
+++ CHANGE.md
@@ -1,7 +1,3 @@
-New version 6.1.17!!
-
 What's fix:
 
-- Numerator/Denominator computing
-- Saving settings
-- Some UI bugs
+- Bug with updating
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.vafeen.universityschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 53
-        versionName = "6.1.18"
+        versionCode = 54
+        versionName = "6.1.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
