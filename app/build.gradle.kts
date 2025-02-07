831b2b80d08345e4adc2bd979ace2b27b5ef0cb4
A <666av6@gmail.com>
2025-02-07 18:19:47 +0300

    New version: 6.1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.vafeen.universityschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "6.1.17"
+        versionCode = 53
+        versionName = "6.1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
